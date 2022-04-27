031f5f0cc0c507b81dcb59332b69ad4da1ec9d8a
Victor <vi350@inbox.ru>
2022-04-27 03:22:16 +0300

    nginx installation added

--- script.sh
+++ script.sh
@@ -18,7 +18,10 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
 rm /root/go1.18.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-cd /home || exit
+sudo apt install nginx
+sudo systemctl enable nginx
+#TODO: wget nginx conf own template
+cd /home || mkdir /home
 mkdir admin
 useradd -s /bin/bash admin
 chown admin:admin /home/admin
